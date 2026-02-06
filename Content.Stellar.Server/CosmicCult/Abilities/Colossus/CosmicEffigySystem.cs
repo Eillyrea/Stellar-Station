7015f42f7ebbbda25ac856f91ba6e71c8a815ac7
pathetic meowmeow <uhhadd@gmail.com>
2026-02-06 10:41:51 -0500

    Goals system (#148)
    * Goals system
    
    * yaml linted
    
    * what if we rename
    
    * fix tests
    
    * add command locs
    
    * fix heisentest

--- Content.Stellar.Server/CosmicCult/Abilities/Colossus/CosmicEffigySystem.cs
+++ Content.Stellar.Server/CosmicCult/Abilities/Colossus/CosmicEffigySystem.cs
@@ -5,15 +5,13 @@
 
 using System.Numerics;
 using Content.Server.Actions;
-using Content.Server.Objectives.Components;
-using Content.Server.Objectives.Systems;
 using Content.Server.Popups;
 using Content.Shared.Maps;
 using Content.Shared.Mind;
 using Content.Shared.Warps;
-using Content.Stellar.Server.CosmicCult.Components;
 using Content.Stellar.Shared.CosmicCult.Components;
 using Content.Stellar.Shared.CosmicCult;
+using Content.Stellar.Shared.Goals;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Map;
 
@@ -22,13 +20,14 @@ namespace Content.Stellar.Server.CosmicCult.Abilities;
 public sealed class CosmicEffigySystem : EntitySystem
 {
     [Dependency] private readonly ActionsSystem _actions = default!;
-    [Dependency] private readonly CodeConditionSystem _codeCondition = default!;
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly TurfSystem _turf = default!;
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly StellarGoalsSystem _goals = default!;
+    [Dependency] private readonly StellarNumericGoalSystem _numericGoal = default!;
 
     public override void Initialize()
     {
@@ -43,7 +42,6 @@ public sealed class CosmicEffigySystem : EntitySystem
             return;
 
         _actions.RemoveAction(ent.Owner, ent.Comp.EffigyPlaceActionEntity);
-        _codeCondition.SetCompleted(ent.Owner, ent.Comp.EffigyObjective);
         Spawn(ent.Comp.EffigyPrototype, pos);
         ent.Comp.Timed = false;
     }
@@ -86,15 +84,30 @@ public sealed class CosmicEffigySystem : EntitySystem
         }
 
         // IF THE OBJECTIVE OR LOCATION IS MISSING, PLACE IT ANYWHERE
-        if (!_mind.TryGetObjectiveComp<CosmicEffigyConditionComponent>(ent, out var obj) || obj.EffigyTarget == null)
-            return true;
-
-        var targetXform = Transform(obj.EffigyTarget.Value);
-        if (xform.MapID != targetXform.MapID || (_transform.GetWorldPosition(xform) - _transform.GetWorldPosition(targetXform)).LengthSquared() > 15 * 15)
+        if (_mind.TryGetMind(ent, out var mind, out _))
         {
-            if (TryComp<WarpPointComponent>(obj.EffigyTarget, out var warp) && warp.Location is not null)
-                _popup.PopupEntity(Loc.GetString("ghost-role-colossus-effigy-error-location", ("LOCATION", warp.Location)), ent, ent);
-            return false;
+            foreach (var goal in _goals.GetGoals<StellarTargetWarpPointGoalComponent>(mind))
+            {
+                if (!TryComp<StellarTargetedGoalComponent>(goal, out var targetedGoal))
+                    continue;
+
+                if (targetedGoal.Target is not { } target)
+                {
+                    _numericGoal.SetCurrent(goal.Owner, 1d);
+                    break;
+                }
+
+                var targetXform = Transform(target);
+                if (xform.MapID != targetXform.MapID || (_transform.GetWorldPosition(xform) - _transform.GetWorldPosition(targetXform)).LengthSquared() > 15 * 15)
+                {
+                    if (TryComp<WarpPointComponent>(target, out var warp) && warp.Location is not null)
+                        _popup.PopupEntity(Loc.GetString("ghost-role-colossus-effigy-error-location", ("LOCATION", warp.Location)), ent, ent);
+                    return false;
+                }
+
+                _numericGoal.SetCurrent(goal.Owner, 1d);
+                break;
+            }
         }
 
         return true;
